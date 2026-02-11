c5e3e31154cecfbf19b1170b185f579ad9a7c3f8
Matheus Marques <134228030+mqrques@users.noreply.github.com>
2026-02-11 01:27:20 -0300

    marquesting

---
+++ script.js
@@ -0,0 +1,9 @@
+const backToTop = document.getElementById('backToTop');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 300) {
+        backToTop.classList.add('show');
+    } else {
+        backToTop.classList.remove('show');
+    }
+});
